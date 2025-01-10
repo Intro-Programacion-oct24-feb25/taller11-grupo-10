643cf2669debc8bfe33a5e0e505d2bce8c50d404
Emilio Poma <ejpoma2@utpl.edu.ec>
2025-01-09 21:05:09 -0500

    Taller 11

--- ejercicios/Problemas/src/problemas/Problema6.java
+++ ejercicios/Problemas/src/problemas/Problema6.java
@@ -10,5 +10,39 @@ package problemas;
  * @author reroes
  */
 public class Problema6 {
-    
+
+    public static void main(String[] args) {
+
+        int[] informacion = {1, 2, 3, 10, 20, 30, 100, 200, 300};
+        double media;
+        double desviacion;
+
+        media = obtenerMedia(informacion);
+        desviacion = obtenerDesviacionEstandar(informacion, media);
+
+        System.out.printf("Media aritmetica: %.2f\n", media);
+        System.out.printf("Desviacion Estandar: %.2f\n", desviacion);
+
+    }
+
+    public static double obtenerMedia(int[] a) {
+        int suma = 0;
+        double promedio;
+        for (int i = 0; i < a.length; i++) {
+            suma = suma + a[i];
+        }
+        promedio = suma / a.length;
+        return promedio;
+    }
+
+    public static double obtenerDesviacionEstandar(int[] a, double media) {
+        double suma = 0;
+        double desviacionEstandar;
+        for (int i = 0; i < a.length; i++) {
+            suma = suma + Math.pow(a[i] - media, 2);
+        }
+        desviacionEstandar = suma / a.length;
+        return desviacionEstandar;
+
+    }
 }
--- ejercicios/Problemas/src/problemas/Problema7.java
+++ ejercicios/Problemas/src/problemas/Problema7.java
@@ -5,10 +5,44 @@
  */
 package problemas;
 
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
  */
 public class Problema7 {
-    
+
+    public static void main(String[] args) {
+
+        String[] arregloCiudades = generarArreglo();
+        imprimirCiudadesCortas(arregloCiudades);
+    }
+
+    public static String[] generarArreglo() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese el número de elementos del arreglo:");
+        int numeroElementos = scanner.nextInt();
+        scanner.nextLine();
+
+        String[] ciudades = new String[numeroElementos];
+
+        for (int i = 0; i <= numeroElementos - 1; i++) {
+            System.out.print("Ingrese el nombre de una ciudad del Ecuador:");
+            ciudades[i] = scanner.nextLine();
+        }
+
+        return ciudades;
+    }
+
+    public static void imprimirCiudadesCortas(String[] arreglo) {
+        System.out.println("Ciudades con 4 o 5 caracteres:");
+        for (int i = 0; i <= arreglo.length - 1; i++) {
+            if (arreglo[i].length() == 4 || arreglo[i].length() == 5) {
+                System.out.println(arreglo[i]);
+            }
+        }
+    }
+
 }
